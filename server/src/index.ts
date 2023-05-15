747bcd0d296cc15279e65548a02ad2970916bbeb
e-kornmann <elton.kornmann@gmail.com>
2023-05-15 02:12:38 +0200

    add the correct http address to cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://tiny-sitters.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://tinysitters.vercel.app'],
   })
 );
 
